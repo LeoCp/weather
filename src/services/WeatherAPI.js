02bdfe68f523c01c28be8309d4f6d157480358c1
Leonardo <leonardocolodette0@gmail.com>
2017-02-27 16:45:33 -0300

    :rocket: :rocket:

--- src/services/WeatherAPI.js
+++ src/services/WeatherAPI.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import * as Consts from './../config/Constants';
 
 const BASE_USER_INFO_API = Consts.IPINFO_BASE_API;
+const BASE_WEATHER_API = Consts.WEATHER_BASE_API;
 const USER_INFO_ENDPOINT = `${BASE_USER_INFO_API}/json`;
+const getWeatherEndpoint = (user) => `${BASE_WEATHER_API}select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${user.city}, ${user.country}") and u='c'&format=json`;
 
-const USER_INFO_API = axios.create({
-  baseURL: BASE_USER_INFO_API,
-  timeout: 600
-});
+const USER_INFO_API = axios.create({ baseURL: BASE_USER_INFO_API });
+const WEATHER_API = axios.create({ baseURL: BASE_WEATHER_API });
 
 const fetchUserInfo = () => {
   return USER_INFO_API
@@ -17,6 +17,16 @@ const fetchUserInfo = () => {
     });
 };
 
+const fetchWeather = (user) => {
+  const WEATHER_ENDPOINT = getWeatherEndpoint(user);
+  return WEATHER_API
+    .get(WEATHER_ENDPOINT)
+    .then((response) => {
+      return response.data;
+    });
+};
+
 export default {
-  fetchUserInfo
+  fetchUserInfo,
+  fetchWeather
 }
